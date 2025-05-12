bd37b112126e5f9c29885d6017e39dcfcfb3db1b
balaram <balaramreddy1710@gmail.com>
2025-05-12 12:46:11 +0530

    [Balaram] Add: Sending HTML file in express

---
+++ Express/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 8000;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "about.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
